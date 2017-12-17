15ce0c78982d33efb3fd5ee3e2c521e38ab3d8d5
BalakinVladislav <balakinv95@gmail.com>
2017-12-17 22:45:35 +0500

    1st try

--- async.js
+++ async.js
@@ -7,7 +7,40 @@ exports.runParallel = runParallel;
  * @param {Array} jobs – функции, которые возвращают промисы
  * @param {Number} parallelNum - число одновременно исполняющихся промисов
  * @param {Number} timeout - таймаут работы промиса
+ * @returns {Promise}
  */
+
 function runParallel(jobs, parallelNum, timeout = 1000) {
     // асинхронная магия
+    return new Promise(resolve => {
+        if (jobs === []) {
+            resolve([]);
+        }
+
+        let jobsDone = 0;
+        let results = [];
+
+        function doJob(job, num) {
+            let current = result => addResult(result, num);
+            Promise.race([
+                job(),
+                new Promise(reject => setTimeout(reject, timeout, new Error('Promise timeout')))])
+                .then(current);
+        }
+
+        function addResult(result, num) {
+            results[num] = result;
+
+            if (results.length === jobs.length) {
+                resolve(results);
+            }
+            if (jobsDone < jobs.length) {
+                doJob(jobs[jobsDone], jobsDone++);
+            }
+        }
+
+        jobs
+            .slice(0, parallelNum)
+            .map(job => doJob(job, jobsDone++));
+    });
 }
